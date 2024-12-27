d32ff10fc8b2236cf6b811475e1ba047a43d1a26
jalaa farhat <jalaa.c.m@gmail.com>
2024-12-27 18:44:03 +0200

    adding style to the projects and adding project card file

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,19 +1,25 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
+import godot from "../../assets/godot.jpg";
+import react from "../../assets/reacttt.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a
-          href="https://github.com/jalaafarhat/Godot-Knight-Game"
-          target="_blank"
-        >
-          <img className="hover" src={viberr} alt="Viberr logo" />
-          <h3>Knight Game</h3>
-          <p>Knight Game using Godot 4</p>
-        </a>
+        <ProjectCard
+          src={godot}
+          link="https://github.com/jalaafarhat/Godot-Knight-Game"
+          h3="Knight Game"
+          p="Knight Game using Godot 4"
+        />
+        <ProjectCard
+          src={react}
+          link="https://github.com/jalaafarhat/jalaa-farhat-portfolio"
+          h3="Portfolio Website"
+          p="Portfolio Website using React"
+        />
       </div>
     </section>
   );
