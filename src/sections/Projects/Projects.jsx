5364dbef0cd54fea6489c1be70a5b7ede82eca82
jalaa farhat <jalaa.c.m@gmail.com>
2025-03-11 10:59:21 +0200

    updating projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,6 @@
 import styles from "./ProjectsStyles.module.css";
-import godot from "../../assets/godot.jpg";
 import react from "../../assets/reacttt.png";
 import ProjectCard from "../../common/ProjectCard";
-import AI from "../../assets/AI.jpg";
 import Movie from "../../assets/movie.jpg";
 
 function projects() {
@@ -11,22 +9,10 @@ function projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={godot}
-          link="https://github.com/jalaafarhat/Godot-Knight-Game"
-          h3="Knight Game"
-          p="Knight Game using Godot 4"
-        />
-        <ProjectCard
           src={react}
           link="https://github.com/jalaafarhat/jalaa-farhat-portfolio"
           h3="Portfolio Website"
           p="Portfolio Website using React"
-        />
-        <ProjectCard
-          src={AI}
-          link="https://github.com/jalaafarhat/AIContentGenerator"
-          h3="AI Content Generator (in progress)"
-          p="AI content generator website using MERN stack"
         />
 
         <ProjectCard
