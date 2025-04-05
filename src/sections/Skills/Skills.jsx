fc4b5983adba5c6fac4f84b8aa48190b91877dee
jalaa farhat <jalaa.c.m@gmail.com>
2025-04-05 15:53:56 +0300

    updating skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,31 +13,26 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="Algorithms and data structure" />
+        <SkillList src={checkMarkIcon} skill="OOP" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Next.js" />
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="MongoDB" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="TailwindCSS" />
         <SkillList src={checkMarkIcon} skill="Bootstrap" />
-      </div>
-      <hr />
-      <br />
-      <h1 className="sectionTitle">Programming Languages</h1>
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Python" />
-        <SkillList src={checkMarkIcon} skill="Java" />
-        <SkillList src={checkMarkIcon} skill="C++" />
-        <SkillList src={checkMarkIcon} skill="C#" />
-        <SkillList src={checkMarkIcon} skill="C" />
       </div>
       <hr />
     </section>
